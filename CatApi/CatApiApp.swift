4fa1b266feae79f5edf9ad6d612e4260260b1e93
Jonathan <jclerence@invorg.com>
2024-10-23 17:38:51 +0530

    Initial Commit

---
+++ CatApi/CatApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatApiApp.swift
+//  CatApi
+//
+//  Created by Hanan Jonathan Clerence on 2024-10-23.
+//
+
+import SwiftUI
+
+@main
+struct CatApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
